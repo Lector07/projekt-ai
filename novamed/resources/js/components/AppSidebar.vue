eeeec8b6a5eb6ed540cb3bdc850c45e2cc8be13b
student <jj131364@stud.ur.edu.pl>
2025-04-29 10:40:18 +0200

    improving colors for dark and light theme

--- novamed/resources/js/components/AppSidebar.vue
+++ novamed/resources/js/components/AppSidebar.vue
@@ -64,7 +64,7 @@ const footerNavItems: NavItem[] = [
         </SidebarContent>
 
         <SidebarFooter >
-            <NavFooter  :items="footerNavItems" />
+            <NavFooter :items="footerNavItems"/>
             <NavUser/>
         </SidebarFooter>
     </Sidebar>
--- novamed/resources/js/components/NavMain.vue
+++ novamed/resources/js/components/NavMain.vue
@@ -37,7 +37,7 @@ function isActive(item: NavItem): boolean {
 
 <template>
     <SidebarGroup class="px-2 py-0">
-        <SidebarGroupLabel>Platform</SidebarGroupLabel>
+        <SidebarGroupLabel class="text-nova-light">Platform</SidebarGroupLabel>
         <SidebarMenu>
             <SidebarMenuItem v-for="item in items" :key="item.title">
                 <SidebarMenuButton
@@ -52,7 +52,7 @@ function isActive(item: NavItem): boolean {
                         class="flex items-center gap-2"
                     >
                         <component v-if="item.icon" :is="item.icon" class="w-5 h-5" />
-                        <span class="text-sm font-medium">{{ item.title }}</span>
+                        <span class="text-sm font-medium ">{{ item.title }}</span>
                     </RouterLink>
 
                     <!-- Dla zewnętrznych linków używamy zwykłego <a> -->
