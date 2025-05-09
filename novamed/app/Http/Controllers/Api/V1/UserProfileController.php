dc26a7e06078e79775178f65a1786051ec58ee2c
student <jj131364@stud.ur.edu.pl>
2025-05-09 17:13:21 +0200

    refactor UpdatePasswordRequest namespace for better organization

--- novamed/app/Http/Controllers/Api/V1/UserProfileController.php
+++ novamed/app/Http/Controllers/Api/V1/UserProfileController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\ProfileUpdateRequest;
-use App\Http\Requests\Api\V1\UpdatePasswordRequest;
+use App\Http\Requests\Api\V1\Auth\UpdatePasswordRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
